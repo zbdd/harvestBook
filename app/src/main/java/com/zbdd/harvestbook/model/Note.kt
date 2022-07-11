aa1566defd5c01b0be5c46dc530930cb2fcee51b
Zac Durber <zdurber@gmail.com>
2022-07-11 14:34:33 +0800

    Added documentation

--- app/src/main/java/com/zbdd/harvestbook/model/Note.kt
+++ app/src/main/java/com/zbdd/harvestbook/model/Note.kt
@@ -4,6 +4,12 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
+/**
+ * Implementation of the INote interface to be used in the View and View Model layers ONLY
+ * Supplied with helper functions for dateTime conversion.
+ *
+ * @author Zac Durber
+ */
 class Note(
     override val id: Int,
     override var title: String?,
