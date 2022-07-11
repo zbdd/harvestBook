00694b9c63d178f1e12f8cb36c04f5bb9d2abfa0
Zac Durber <zdurber@gmail.com>
2022-07-11 14:33:32 +0800

    Added documentation

--- app/src/main/java/com/zbdd/harvestbook/model/NoteRepository.kt
+++ app/src/main/java/com/zbdd/harvestbook/model/NoteRepository.kt
@@ -4,10 +4,20 @@ import com.zbdd.harvestbook.model.room.AppDatabase
 import com.zbdd.harvestbook.model.room.NoteEntity
 import kotlinx.coroutines.runBlocking
 
+/**
+ * The implementation of the INoteRepository interface that wraps the expected requests to the underlying
+ * database for compatibility.
+ * Each call utilises coroutines to protect the main thread the rest of the app runs on.
+ *
+ * @author Zac Durber
+ */
 class NoteRepository: INoteRepository {
     private val DB = AppDatabase.getInstance()
     private val dao = DB.getNoteDAO()
 
+    /**
+     * Convert from the upper level INote object to the RoomDB compatible NoteEntity
+     */
     fun noteToEntity(entry: INote): NoteEntity {
         return NoteEntity(entry.id, entry.title, entry.content, entry.dateTime, entry.updated)
     }
